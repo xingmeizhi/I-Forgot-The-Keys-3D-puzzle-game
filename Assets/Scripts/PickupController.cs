dce83436323b488e179d647c80fffac4d8b11d8b
jaimanbharadwa <60994055+jaimanbharadwa@users.noreply.github.com>
2023-02-16 16:25:06 -0500

    Add files via upload

---
+++ Assets/Scripts/PickupController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+
+    public float pickupRange = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(transform.position,
+                transform.TransformDirection(Vector3.forward), out RaycastHit hit, pickupRange))
+            {
+                PickupObject(hit.transform.gameObject);
+            }
+        }
+    }
+
+    void PickupObject(GameObject pickObj)
+    {
+        if (pickObj.GetComponent<Rigidbody>())
+        {
+            Destroy(pickObj);
+        }
+    }
+}
