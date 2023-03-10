06523d3115ac73948270bf1328b8fd912757db31
xingmeizhi <92602862+xingmeizhi@users.noreply.github.com>
2023-03-10 02:29:14 -0500

    Added animation and sound effect

---
+++ Assets/Scripts/FridgeAnimation.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeAnimation : MonoBehaviour
+{
+
+
+    public float raycastDistance = 10f;
+
+    public Animator animFridge;
+
+    public GameObject targetObject;
+
+    public AudioSource Fridgeopenaudio;
+    public AudioSource Fridgecloseaudio;
+
+    private bool open = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && IsPlayerNear() && open)
+        {
+            animFridge.Play("FridgeClose");
+            open = false;
+            Fridgecloseaudio.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && IsPlayerNear() && !open)
+        {
+            animFridge.Play("FridgeOpen");
+            open = true;
+            Fridgeopenaudio.Play();
+        }
+
+        bool IsPlayerNear()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, raycastDistance))
+            {
+                if (hit.collider.gameObject == targetObject)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
