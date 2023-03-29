13922f0ac7eb438225b2ad05a7d0fd8cadbaa952
aulin1 <audrey.youran.lin@gmail.com>
2023-03-29 09:38:13 -0400

    Started Adding Level 2 Functionality

---
+++ Assets/Scripts/ReplaceObjectScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceObjectScript : MonoBehaviour
+{
+    public GameObject replace;
+    // Start is called before the first frame update
+    void Start()
+    {
+        replace.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
