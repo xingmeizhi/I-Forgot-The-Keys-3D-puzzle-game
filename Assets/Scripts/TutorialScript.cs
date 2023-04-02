f46b12da971fc7d0143cfa58748df92cb824e85b
aulin1 <audrey.youran.lin@gmail.com>
2023-04-02 10:18:31 -0400

    Added stuff

---
+++ Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q)){
+            gameObject.SetActive(false);
+        }
+    }
+}
