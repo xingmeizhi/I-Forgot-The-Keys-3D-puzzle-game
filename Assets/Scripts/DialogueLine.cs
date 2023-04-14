424eb5de32ef27468a75b09a37dda2a1796cabac
xingmeizhi <92602862+xingmeizhi@users.noreply.github.com>
2023-04-14 01:28:33 -0400

    cutscenes

---
+++ Assets/Scripts/DialogueLine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//Learn the code follow the tutorial from https://www.youtube.com/watch?v=8eJ_gxkYsyY 
+//Undertale DIALOGUE|CUTSCENE in Unity (Episode 1)
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private Text textHolder;
+
+        [Header ("Text Options")]
+        [SerializeField] private string input;
+        [SerializeField] private Color textColor;
+        [SerializeField] private Font textFont;
+
+        [Header("Time parameters")]
+        [SerializeField] private float delay;
+        [SerializeField] private float delayBetweenLines;
+
+        [Header("Sound")]
+        [SerializeField] private AudioClip sound;
+
+        private void Awake()
+        {
+            textHolder = GetComponent<Text>();
+            textHolder.text = "";
+
+        }
+
+        private void Start()
+        {
+            StartCoroutine(WriteText(input, textHolder, textColor, textFont, delay, sound, delayBetweenLines));
+        }
+    }
+}
